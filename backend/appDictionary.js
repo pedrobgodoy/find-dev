7355fb3db875645159a6447a9ea9bb8b51b78459
Pedro Henrique <pedrohenrique.godoy@outlook.com>
2020-01-30 22:51:12 -0300

    Initial Commmit, adding backend

---
+++ backend/appDictionary.js
@@ -0,0 +1,5 @@
+const rainbowSix = {id: "1", name: "RainbowSix", path: "D:/Program Files (x86)/Steam/steamapps/common/Tom Clancy's Rainbow Six Siege/RainbowSix.exe"};
+
+const apps = [rainbowSix];
+
+module.exports = {apps};
---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const express  = require('express');
+const dotenv   = require('dotenv');
+const routes   = require('./routes.js');
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00-nzrjv.gcp.mongodb.net:27017,cluster0-shard-00-01-nzrjv.gcp.mongodb.net:27017,cluster0-shard-00-02-nzrjv.gcp.mongodb.net:27017/${process.env.DB_NAME}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(process.env.PORT);
